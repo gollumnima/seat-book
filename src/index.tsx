0d232ab3d5b6f9c03a7ce6b72b67c85a5630a476
Doori Kim <doori.alice.kim@gmail.com>
2022-04-12 23:32:15 +0900

    [Chore] apply the latest rule

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,7 @@
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root'),
-);
+const container = document.getElementById('root') as HTMLElement;
+const root = createRoot(container);
+
+root.render(<App />);
